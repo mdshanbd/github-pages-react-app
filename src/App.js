c3c702306921077b0470dfad90c51138dfd0c80e
JahidulAlam <68185607+JahidulAlam@users.noreply.github.com>
2020-11-09 12:13:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React jishan
+          Learn React with jishan
         </a>
       </header>
     </div>
